4a81886bc454ce20194755f02900081f698f9e04
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2026-06-24 15:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n <= 1){
-            return n;
+
+    int fibo(int n , vector<int>& dp){
+        if(n == 0){
+            return 0;
         }
-        return fib(n-1) + fib(n-2);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int t = 2; t <= n; t++){
+            dp[t] = dp[t-1] + dp[t-2];
+        }
+        
+        return dp[n];
+    }
+
+    int fib(int n) {
+        vector<int> dp(n+1);
+        return fibo(n, dp);
     }
 };
