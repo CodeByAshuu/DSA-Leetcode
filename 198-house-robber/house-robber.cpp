f5ad0caacca20de9da4137bacd20e567cc4073ae
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2026-07-19 16:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int maxAmount(int n, vector<int>& nums, vector<int>& dp){
-        if(n== 1) return nums[0];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-        }
-
-        return dp[n-1];
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n);
-        return maxAmount(n, nums, dp);
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for(int i = 1; i < n; i++){
+            int pick =  nums[i] + prev2;
+            int nopick = prev1;
+            int curr = max(pick, nopick);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 };
