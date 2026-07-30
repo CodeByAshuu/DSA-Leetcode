9f0cef5d80fefe8084e7714a8c7813eb16b632f2
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2026-07-30 12:15:32 +0530

    Time: 157 ms (5.01%) | Memory: 113.8 MB (5.99%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s;
-        for(int i = 0; i < nums.size(); i++){
-            if(s.find(nums[i]) !=  s.end()){
+        map<int,int> mp;
+
+        for(int x : nums){
+            if(mp.find(x) != mp.end()){
                 return true;
             }
-
-            s.insert(nums[i]);
+            mp[x]++;
         }
+
         return false;
     }
 };
